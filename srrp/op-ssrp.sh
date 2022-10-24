e0375872176e42adf555c51a4d8cff7b3958d1d5
66o99 <116540988+66o99@users.noreply.github.com>
2022-10-24 23:57:45 +0800

    Create op-ssrp.sh

---
+++ srrp/op-ssrp.sh
@@ -0,0 +1,7 @@
+opkg remove  dnsmasq
+opkg update
+opkg install dnsmasq-full --force-depends
+opkg install unzip luci-compat wget luci-i18n-base-zh-cn
+wget https://66o99.github.io/ssrp/ssrp.zip
+unzip -o ssrp.zip
+opkg install *.ipk
