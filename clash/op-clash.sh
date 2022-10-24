4b1f21a506f4624d3575049505f011677cd79dd6
66o99 <116540988+66o99@users.noreply.github.com>
2022-10-25 00:01:13 +0800

    Create op-clash.sh

---
+++ clash/op-clash.sh
@@ -0,0 +1,5 @@
+opkg remove  dnsmasq
+opkg update
+opkg install dnsmasq-full --force-depends
+opkg install wget coreutils coreutils-nohup bash curl ca-certificates ipset ip-full iptables-mod-tproxy kmod-tun luci-compat luci-i18n-base-zh-cn
+opkg install http://github.com/vernesong/OpenClash/releases/download/v0.45.59-beta/luci-app-openclash_0.45.59-beta_all.ipk
