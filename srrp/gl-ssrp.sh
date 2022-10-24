1e1aa2a6b6539d2f7157813892b94e292aabb3f8
66o99 <116540988+66o99@users.noreply.github.com>
2022-10-25 00:16:32 +0800

    Delete srrp directory

--- srrp/gl-ssrp.sh
+++
@@ -1,5 +0,0 @@
-opkg update
-opkg install wget luci-compat unzip luci-i18n-base-zh-cn
-wget https://66o99.github.io/ssrp/ssrp.zip
-unzip -o ssrp.zip
-opkg install *.ipk
--- srrp/op-ssrp.sh
+++
@@ -1,7 +0,0 @@
-opkg remove  dnsmasq
-opkg update
-opkg install dnsmasq-full --force-depends
-opkg install unzip luci-compat wget luci-i18n-base-zh-cn
-wget https://66o99.github.io/ssrp/ssrp.zip
-unzip -o ssrp.zip
-opkg install *.ipk
