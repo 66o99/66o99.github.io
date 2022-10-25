bb9611b2f8252844b5dc7a5a477ad8558817aacb
66o99 <116540988+66o99@users.noreply.github.com>
2022-10-25 13:19:05 +0800

    Update gl-clash.sh

--- clash/gl-clash.sh
+++ clash/gl-clash.sh
@@ -1,5 +1,5 @@
 opkg update
 opkg install luci-i18n-base-zh-cn wget coreutils-nohup bash luci-compat
-opkg install http://downloads.openwrt.org/releases/21.02.0/packages/mipsel_24kc/packages/libcap_2.43-1_mipsel_24kc.ipk
-opkg install http://downloads.openwrt.org/releases/21.02.0/packages/mipsel_24kc/packages/libcap-bin_2.43-1_mipsel_24kc.ipk
+opkg install http://66o99.github.io/clash/libcap_2.43-1_mipsel_24kc.ipk
+opkg install http://66o99.github.io/clash/libcap-bin_2.43-1_mipsel_24kc.ipk
 opkg install http://github.com/vernesong/OpenClash/releases/download/v0.45.59-beta/luci-app-openclash_0.45.59-beta_all.ipk
