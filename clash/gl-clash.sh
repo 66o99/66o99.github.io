08c1a092828600ce40192daf0d69cb8091898e15
66o99 <116540988+66o99@users.noreply.github.com>
2022-10-27 19:37:46 +0800

    Update gl-clash.sh

--- clash/gl-clash.sh
+++ clash/gl-clash.sh
@@ -2,4 +2,5 @@ opkg update
 opkg install luci-i18n-base-zh-cn wget coreutils-nohup bash luci-compat
 opkg install http://66o99.github.io/clash/libcap_2.43-1_mipsel_24kc.ipk
 opkg install http://66o99.github.io/clash/libcap-bin_2.43-1_mipsel_24kc.ipk
-opkg install http://github.com/vernesong/OpenClash/releases/download/v0.45.59-beta/luci-app-openclash_0.45.59-beta_all.ipk
+opkg install http://66o99.github.io/clash/luci-app-openclash_0.45.59-beta_all.ipk
+#opkg install http://github.com/vernesong/OpenClash/releases/download/v0.45.59-beta/luci-app-openclash_0.45.59-beta_all.ipk
