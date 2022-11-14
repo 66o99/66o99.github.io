2408be6512a65ff1a78afc0095e65e1586337536
66o99 <116540988+66o99@users.noreply.github.com>
2022-11-14 15:52:03 +0800

    Update op-clash.sh

--- clash/op-clash.sh
+++ clash/op-clash.sh
@@ -5,3 +5,4 @@ opkg install wget coreutils coreutils-nohup bash curl ca-certificates ipset ip-f
 #opkg install http://66o99.github.io/clash/luci-app-openclash_0.45.59-beta_all.ipk
 wget https://ghproxy.com/https://github.com/vernesong/OpenClash/releases/download/v0.45.59-beta/luci-app-openclash_0.45.59-beta_all.ipk
 opkg install luci-app-openclash_*.ipk
+rm -rf luci-app-openclash_*.ipk
