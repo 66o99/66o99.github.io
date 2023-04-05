ee86e7cac090cfcd9074e69f017e1318cf955995
66o99 <116540988+66o99@users.noreply.github.com>
2023-04-05 15:16:30 +0800

    Update and rename a.txt to ssrp-full.sh

---
+++ ssrp-full/ssrp-full.sh
@@ -0,0 +1,6 @@
+opkg update
+opkg install wget luci luci-compat unzip luci-i18n-base-zh-cn
+wget https://66o99.github.io/ssrp-full/ssrp-full.zip
+unzip -o ssrp-full.zip
+opkg install *.ipk
+rm -rf ssrp-full.zip *-*.ipk
