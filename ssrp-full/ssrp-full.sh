e76d625d35fdca98df3259bc4a14da54fb3f3b90
66o99 <116540988+66o99@users.noreply.github.com>
2023-04-05 15:18:22 +0800

    Update ssrp-full.sh

--- ssrp-full/ssrp-full.sh
+++ ssrp-full/ssrp-full.sh
@@ -1,5 +1,5 @@
 opkg update
-opkg install wget luci luci-compat unzip luci-i18n-base-zh-cn
+#opkg install wget luci luci-compat unzip luci-i18n-base-zh-cn
 wget https://ghproxy.com/https://66o99.github.io/ssrp-full/ssrp-full.zip
 unzip -o ssrp-full.zip
 opkg install *.ipk
