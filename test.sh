bada6ce5fe6d1329ad6abfaeffe93151e57fdfaf
66o99 <116540988+66o99@users.noreply.github.com>
2023-04-07 17:41:21 +0800

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,11 @@
+cd /tmp && opkg update
+#opkg install wget luci luci-compat unzip luci-i18n-base-zh-cn
+if [ $(sed -n '7p' /etc/banner | cut -c 10,11,13,14) = 1907 ];then
+#echo "Yes!"
+opkg install coreutils coreutils-base64 iptables-mod-tproxy libuci-lua libudns libmbedtls12
+else
+#echo "No...."
+wget https://ghproxy.com/https://github.com/66o99/66o99.github.io/blob/main/ssrp-full/ssrp-full.zip
+unzip -o ssrp-full.zip
+opkg install *.ipk
+rm -rf ssrp-full.zip *-*.ipk
