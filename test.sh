073ece78d5f9144f87c611f70e768d636aa4cd12
66o99 <116540988+66o99@users.noreply.github.com>
2023-04-07 19:14:39 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,54 @@
+cat << EOF
+ -----------------------------------------------------
+  1 = install_ShadowSocksR+     2 = install_OpenClash 
+ -----------------------------------------------------
+EOF
+echo "1/2"
+read input
+
+  if [ $input = 1 ];then
+  echo "ok ! install - ShadowSocksR - waiting..."
+
 cd /tmp && opkg update
-#opkg install wget luci luci-compat unzip luci-i18n-base-zh-cn
 if [ $(sed -n '7p' /etc/banner | cut -c 10,11,13,14) = 1907 ];then
 echo "Yes!"
 opkg install coreutils coreutils-base64 iptables-mod-tproxy libuci-lua libudns libmbedtls12
+
 else
 echo "No...."
 fi
+
 wget https://ghproxy.com/https://github.com/66o99/66o99.github.io/blob/main/ssrp-full/ssrp-full.zip
 unzip -o ssrp-full.zip
 opkg install *.ipk
 rm -rf ssrp-full.zip *-*.ipk
+
+  elif [ $input = 2 ];then
+  echo "ok ! install - OpenClash - waiting....."
+
+cd /tmp && opkg update
+if [ $(sed -n '7p' /etc/banner | cut -c 10,11,13,14) = 1907 ];then
+echo "Yes!"
+opkg install luci luci-i18n-base-zh-cn wget coreutils-nohup bash luci-compat
+opkg install http://66o99.github.io/clash/libcap_2.43-1_mipsel_24kc.ipk
+opkg install http://66o99.github.io/clash/libcap-bin_2.43-1_mipsel_24kc.ipk
+
+else
+echo "No...."
+opkg remove  dnsmasq
+opkg install dnsmasq-full --force-depends
+opkg install wget coreutils coreutils-nohup bash curl ca-certificates ipset ip-full iptables-mod-tproxy kmod-tun luci-compat luci-i18n-base-zh-cn
+fi
+
+wget https://ghproxy.com/https://github.com/vernesong/OpenClash/releases/download/v0.45.59-beta/luci-app-openclash_0.45.59-beta_all.ipk
+opkg install luci-app-openclash_*.ipk
+rm -rf luci-app-openclash_*.ipk
+
+  else
+  echo "ok ! install - End ."
+  fi
+
+
+
+
+
