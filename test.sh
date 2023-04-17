9938c14940ca8b2e2f0e3fdc2759e3ff90125e42
66o99 <116540988+66o99@users.noreply.github.com>
2023-04-17 14:55:51 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -26,8 +26,9 @@ opkg install luci luci-i18n-base-zh-cn wget coreutils-nohup bash luci-compat
 opkg install http://66o99.github.io/clash/libcap_2.43-1_mipsel_24kc.ipk
 opkg install http://66o99.github.io/clash/libcap-bin_2.43-1_mipsel_24kc.ipk
 else
-opkg remove  dnsmasq && opkg install dnsmasq-full --force-depends
-opkg install wget coreutils coreutils-nohup bash curl ca-certificates ipset ip-full iptables-mod-tproxy kmod-tun luci-compat luci-i18n-base-zh-cn
+#opkg remove  dnsmasq && opkg install dnsmasq-full --force-depends
+#opkg install wget coreutils coreutils-nohup bash curl ca-certificates ipset ip-full iptables-mod-tproxy kmod-tun
+opkg luci-compat luci-i18n-base-zh-cn
 fi
 wget https://ghproxy.com/https://github.com/vernesong/OpenClash/releases/download/v0.45.59-beta/luci-app-openclash_0.45.59-beta_all.ipk
 opkg install luci-app-openclash_*.ipk && rm -rf luci-app-openclash_*.ipk
