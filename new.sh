69e7f2bfeb6045cdc9cc48aff602495244eb3cc4
66o99 <116540988+66o99@users.noreply.github.com>
2023-06-16 11:34:35 +0800

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Command：wget https://66o99.github.io/new.sh && /bin/sh new.sh
-rm -rf test.sh && cd /tmp/tmp && opkg update && clear
+rm -rf new.sh && cd /tmp/tmp && opkg update && clear
 cat << EOF
  ---For Mipsel_24Kc-----------------------------------
   1 = ShadowSocksR+  2 = OpenClash   < --  Full   - >
